4bc698fe5a4a7254da31a75e76052a3ecb157ed3
Kurtis Blankenship <kblankenship1989@gmail.com>
2020-04-12 22:12:36 -0500

    add weekdays

--- MMM-nutrislice-menu.js
+++ MMM-nutrislice-menu.js
@@ -173,6 +173,19 @@ Module.register("MMM-nutrislice-menu", {
 		return mapOfDays;
 	  },
 
+	getWeekDay(dateString) {
+		const date = new Date(dateString);
+		var weekday = new Array(7);
+		weekday[0] = "Sunday";
+		weekday[1] = "Monday";
+		weekday[2] = "Tuesday";
+		weekday[3] = "Wednesday";
+		weekday[4] = "Thursday";
+		weekday[5] = "Friday";
+		weekday[6] = "Saturday";
+		return weekday[date.getDay()];
+	},
+
 	getScripts: function() {
 		return [];
 	},
