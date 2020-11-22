dc18dcfda50f0fbde829c46c37ee2abeaeab1f7d
Kurtis Blankenship <kblankenship1989@gmail.com>
2020-11-22 15:33:12 -0600

    try to isolate get data reply

--- node_helper.js
+++ node_helper.js
@@ -31,22 +31,22 @@ module.exports = NodeHelper.create({
 		//var myUrl = this.config.apiBase + this.config.requestURL + '?hafasID=' + this.config.stationID + '&time=' + currentDate;
 		myUrl = "https://pleasantvalley.nutrislice.com/menu/api/weeks/school/elementary/menu-type/lunch/2020/10/12/?format=json";
 
-		console.log("data request started console.log from node_helper");
-		Log.info("data request started Log.info from node_helper");
+		//console.log("data request started console.log from node_helper");
+		//Log.info("data request started Log.info from node_helper");
 
 		request({
 			url: myUrl,
 			method: "GET"
 			//headers: { 'RNV_API_TOKEN': this.config.apiKey }
 		}, function (error, response, body) {
-			console.log("nutrislice response code: " + response.statusCode);
+			//console.log("nutrislice response code: " + response.statusCode);
 			if (!error && response.statusCode == 200) {
 				self.sendSocketNotification("DATA", body);
 			} else {
 				self.sendSocketNotification("STATUSERROR", response.statusCode);
 			}
 		});
-
+		self.sendSocketNotification("GETDATATIMEOUT", true);
 		setTimeout(function() { self.getData(); }, this.config.retryDelay);
 	},
 
